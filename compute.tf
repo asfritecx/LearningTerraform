40d3b0f06c601dc68e3ffa80a4dab721adbc95cb
Jason <asphyticz@outlook.com>
2023-02-25 09:44:32 +0800

    change default size

--- compute.tf
+++ compute.tf
@@ -18,7 +18,7 @@ resource "azurerm_windows_virtual_machine" "LAPP01" {
   os_disk {
     
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {
@@ -53,7 +53,7 @@ resource "azurerm_windows_virtual_machine" "LDB01" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {
